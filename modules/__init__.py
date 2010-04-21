1633b9a1ace74a5a7cbf445ce7ceb790d0411e79
Greg <gjgaughan@googlemail.com>
2010-04-21 21:34:32 +0100

    Add comment about module naming

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,8 @@
 #pipe2py modules package
 #Author: Greg Gaughan
 
+#Note: each module name must match the name used internally by Yahoo, preceded by pipe
+
 __all__ = ['pipefetch',
            'pipefetchdata',
            'pipedatebuilder',
