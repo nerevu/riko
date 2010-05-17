4627f3d58131f31d926d7dec8d93337e11c9d9a2
Greg <gjgaughan@googlemail.com>
2010-05-17 21:37:56 +0100

    Add complex twitter pipeline for testing

--- test/testbasics.py
+++ test/testbasics.py
@@ -153,6 +153,18 @@ class TestBasics(unittest.TestCase):
             count += 1
             
         self.assertTrue(count > 0)
+    
+    def test_twitter(self):
+        """Loads a pipeline containing a loop, complex regex etc. for twitter
+        """
+        pipe_def = self._get_pipe_def("pipe_ac45e9eb9b0174a4e53f23c4c9903c3f.json")
+        p = pipe2py.compile.parse_and_build_pipe(self.context, pipe_def)
+        
+        #todo: check the data! e.g. pubdate etc.
+        count = 0
+        for i in p:
+            count += 1
+            
         
         
     #todo test malformed pipeline syntax too
