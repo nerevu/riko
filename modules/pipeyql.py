02483546e2d6e134689a0d746025c4256279c05d
Greg <greg.gaughan@isotoma.com>
2010-10-14 13:06:37 +0100

    Allow YQL module to be embedded in a loop

--- modules/pipeyql.py
+++ modules/pipeyql.py
@@ -23,26 +23,32 @@ def pipe_yql(context, _INPUT, conf,  **kwargs):
     """
     url = "http://query.yahooapis.com/v1/public/yql" #todo get from a config/env file
     
-    yql = util.get_value(conf['yqlquery'], kwargs)
+    for item in _INPUT:
+        if "subkey" in conf['yqlquery']:
+            yql = item[conf['yqlquery']['subkey']]
+        else:
+            yql = util.get_value(conf['yqlquery'], kwargs)
+        
+        query = urllib.urlencode({'q':yql,
+                                  #note: we use the default format of xml since json loses some structure
+                                  #todo diagnostics=true e.g. if context.test
+                                  #todo consider paging for large result sets
+                                 })
+        req = urllib2.Request(url, query)    
+        response = urllib2.urlopen(req)    
+        
+        #Parse the response
+        ft = ElementTree.parse(response)
+        if context.verbose:
+            print "pipe_yql loading xml:", yql
+        root = ft.getroot()
+        #note: query also has row count
+        results = root.find('results')
+        #Convert xml into generation of dicts
+        for element in results.getchildren():
+            i = util.xml_to_dict(element)
+            yield i
     
-    query = urllib.urlencode({'q':yql,
-                              #note: we use the default format of xml since json loses some structure
-                              #todo diagnostics=true e.g. if context.test
-                              #todo consider paging for large result sets
-                             })
-    req = urllib2.Request(url, query)    
-    response = urllib2.urlopen(req)    
-    
-    #Parse the response
-    ft = ElementTree.parse(response)
-    if context.verbose:
-        print "pipe_yql loading xml:", yql
-    root = ft.getroot()
-    #note: query also has row count
-    results = root.find('results')
-    #Convert xml into generation of dicts
-    for element in results.getchildren():
-        i = util.xml_to_dict(element)
-        yield i
-    
+        if item == True: #i.e. this is being fed forever, i.e. not in a loop, so we just yield our item once
+            break
     
