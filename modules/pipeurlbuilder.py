cf806d9818ce8912559e321e8e2f8f550a971560
Tuukka Hastrup <Tuukka.Hastrup@iki.fi>
2011-01-04 20:42:47 +0200

    ignore None path elements, make PATH optional in urlbuilder pipe module

--- modules/pipeurlbuilder.py
+++ modules/pipeurlbuilder.py
@@ -26,13 +26,14 @@ def pipe_urlbuilder(context, _INPUT, conf, **kwargs):
         if not url.endswith('/'):
             url += '/'
         
-        path = conf['PATH']
-        if not isinstance(path, list):
-            path = [path]
-        path = [util.get_value(p, item, **kwargs) for p in path]
-        
-        url += "/".join(path)
-        url = url.rstrip("/")
+        if 'PATH' in conf: 
+            path = conf['PATH']
+            if not isinstance(path, list):
+                path = [path]
+            path = [util.get_value(p, item, **kwargs) for p in path if p]
+
+            url += "/".join(p for p in path if p)
+            url = url.rstrip("/")
         
         #Ensure url is valid
         url = urllib2.quote(url, safe="%/:=&?~#+!$,;'@()*[]")
