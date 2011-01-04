b2939af82ec2673a7b43763ac6830743b0166dcb
Tuukka Hastrup <Tuukka.Hastrup@iki.fi>
2011-01-04 20:39:38 +0200

    add loop support to datebuilder pipe module

--- modules/pipedatebuilder.py
+++ modules/pipedatebuilder.py
@@ -1,20 +1,24 @@
 # pipedatebuilder.py
 #
 
+from pipe2py import util
+
 def pipe_datebuilder(context, _INPUT, conf, **kwargs):
     """This source builds a date and yields it forever.
     
     Keyword arguments:
     context -- pipeline context
-    _INPUT -- not used
+    _INPUT -- XXX
     conf:
         DATE -- date
     
     Yields (_OUTPUT):
     date
     """
-    date = conf['DATE']['value']
-    
-    while True:
+    for item in _INPUT:
+        date = util.get_value(conf['DATE'], item, **kwargs)
+
         yield date
 
+        if item == True: #i.e. this is being fed forever, i.e. not in a loop, so we just yield our item once
+            break
