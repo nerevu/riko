aa095ef70f6d5aedcf97a63c73f39491faffaeca
Greg <gjgaughan@googlemail.com>
2011-01-21 20:49:02 +0000

    Strip trailing / even if no path given

--- modules/pipeurlbuilder.py
+++ modules/pipeurlbuilder.py
@@ -33,7 +33,7 @@ def pipe_urlbuilder(context, _INPUT, conf, **kwargs):
             path = [util.get_value(p, item, **kwargs) for p in path if p]
 
             url += "/".join(p for p in path if p)
-            url = url.rstrip("/")
+        url = url.rstrip("/")
         
         #Ensure url is valid
         url = urllib2.quote(url, safe="%/:=&?~#+!$,;'@()*[]")
