7e1813a310171ec8868857bcc8c5df85dd9b6afc
Greg <gjgaughan@googlemail.com>
2011-05-04 07:55:50 +0100

    Only match to_delimiter after from_delimiter and allow module to appear within a loop

--- modules/pipefetchpage.py
+++ modules/pipefetchpage.py
@@ -24,75 +24,86 @@ def pipe_fetchpage(context, _INPUT, conf, **kwargs):
         - don't retrieve if page is not indexable.
         - item delimiter removes the closing tag if using a HTML tag
           (not documented but happens)
+        - items should be cleaned, i.e. stripped of HTML tags
     """
-    url = util.get_value(conf['URL'], _INPUT, **kwargs)
-    if context.verbose:
-        print "FetchPage: Preparing to download:",url
+    urls = conf['URL']
+    if not isinstance(urls, list):
+        urls = [urls]
 
-    try:
-        request = urllib2.Request(url)
-        request.add_header('User-Agent','Yahoo Pipes 1.0')
-        request = urllib2.build_opener().open(request)
-        content = unicode(request.read(),
-                          request.headers['content-type'].split('charset=')[-1])
-
-        # TODO it seems that Yahoo! converts relative links to absolute
-        # TODO this needs to be done on the content but seems to be a non-trival
-        # TODO task python?
-
-        if context.verbose:
-            print "............FetchPage: content ................."
-            print content.encode("utf-8")
-            print "............FetchPage: EOF     ................."
-
-        from_delimiter = util.get_value(conf["from"], _INPUT, **kwargs)
-        to_delimiter = util.get_value(conf["to"], _INPUT, **kwargs)
-        split_token = util.get_value(conf["token"], _INPUT, **kwargs)
-
-        # determine from location, i.e. from where to start reading content
-        from_location = 0
-        if from_delimiter != "":
-            from_location = content.find(from_delimiter)
-            # Yahoo! does not strip off the from_delimiter.
-            #if from_location > 0:
-            #    from_location += len(from_delimiter)
-
-        # determine to location, i.e. where to stop reading content
-        to_location = 0
-        if to_delimiter != "":
-            to_location = content.find(to_delimiter)
-
-        # reduce the content depended on the to/from locations
-        if from_location > 0 and to_location > 0:
-            content = content[from_location:to_location]
-        elif from_location > 0:
-            content = content[from_location:]
-        elif to_location > 0:
-            content = content[:to_location]
-
-        # determine items depended on the split_token
-        items = []
-        if split_token != "":
-            items = content.split(split_token)
-        else:
-            items = [content]
-
-        if context.verbose:
-            print "FetchPage: found count items:",len(items)
-
-        for item in items:
+    for item in _INPUT:
+        for item_url in urls:
+            url = util.get_value(item_url, item, **kwargs)
             if context.verbose:
-                print "--------------item data --------------------"
-                print item
-                print "--------------EOF item data ----------------"
-            yield dict( { "content" : item } )
+                print "FetchPage: Preparing to download:",url
+                
+            try:
+                request = urllib2.Request(url)
+                request.add_header('User-Agent','Yahoo Pipes 1.0')
+                request = urllib2.build_opener().open(request)
+                content = unicode(request.read(),
+                                  request.headers['content-type'].split('charset=')[-1])
+        
+                # TODO it seems that Yahoo! converts relative links to absolute
+                # TODO this needs to be done on the content but seems to be a non-trival
+                # TODO task python?
+        
+                if context.verbose:
+                    print "............FetchPage: content ................."
+                    print content.encode("utf-8")
+                    print "............FetchPage: EOF     ................."
+        
+                from_delimiter = util.get_value(conf["from"], _INPUT, **kwargs)
+                to_delimiter = util.get_value(conf["to"], _INPUT, **kwargs)
+                split_token = util.get_value(conf["token"], _INPUT, **kwargs)
+        
+                # determine from location, i.e. from where to start reading content
+                from_location = 0
+                if from_delimiter != "":
+                    from_location = content.find(from_delimiter)
+                    # Yahoo! does not strip off the from_delimiter.
+                    #if from_location > 0:
+                    #    from_location += len(from_delimiter)
+        
+                # determine to location, i.e. where to stop reading content
+                to_location = 0
+                if to_delimiter != "":
+                    to_location = content.find(to_delimiter, from_location)
+        
+                # reduce the content depended on the to/from locations
+                if from_location > 0 and to_location > 0:
+                    content = content[from_location:to_location]
+                elif from_location > 0:
+                    content = content[from_location:]
+                elif to_location > 0:
+                    content = content[:to_location]
+        
+                # determine items depended on the split_token
+                res_items = []
+                if split_token != "":
+                    res_items = content.split(split_token)
+                else:
+                    res_items = [content]
+        
+                if context.verbose:
+                    print "FetchPage: found count items:",len(res_items)
+        
+                for res_item in res_items:
+                    if context.verbose:
+                        print "--------------item data --------------------"
+                        print res_item
+                        print "--------------EOF item data ----------------"
+                    yield dict( { "content" : res_item } )
+        
+            except Exception, e:
+                if context.verbose:
+                    print "FetchPage: failed to retrieve from:", url
+        
+                    print "----------------- FetchPage -----------------"
+                    import traceback
+                    traceback.print_exc()
+                    print "----------------- FetchPage -----------------"
+                raise
 
-    except Exception, e:
-        if context.verbose:
-            print "FetchPage: failed to retrieve from:", url
-
-            print "----------------- FetchPage -----------------"
-            import traceback
-            traceback.print_exc()
-            print "----------------- FetchPage -----------------"
-        raise
+        if item == True: #i.e. this is being fed forever, i.e. not in a loop, so we just yield our item once
+            break
+            
