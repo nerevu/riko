954db3fc3f91ed918fe10043b6bb9aa4e1d8542f
Greg <gjgaughan@googlemail.com>
2011-08-29 15:12:15 +0100

    Add setup file for PyPI registration

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pipe2py',
+      version='1.0',
+      description='A project to compile Yahoo! Pipes into Python',
+      author='Greg Gaughan',
+      author_email='gjgaughan@gmail.com',
+      url='https://github.com/ggaughan/pipe2py',
+      packages=['pipe2py',],
+     )
+
