9b0bc63032319256fe5f74c8c707fa3419335206
Greg <gjgaughan@googlemail.com>
2012-04-11 20:56:22 +0100

    Updated setup descriptions

--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@
 
 from distutils.core import setup
 
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='pipe2py',
       version='0.9.4',
-      description='A project to compile Yahoo! Pipes into Python',
+      description=('A project to compile Yahoo! Pipes into Python. '
+                   'The pipe2py package can compile a Yahoo! Pipe into pure Python source code, '
+                   'or it can interpret the pipe on-the-fly. It supports embedded pipes too.'
+                  ), 
       author='Greg Gaughan',
       author_email='gjgaughan@gmail.com',
-      url='https://github.com/ggaughan/pipe2py',
+      url='http://ggaughan.github.com/pipe2py/',
+      license = 'GPL2',
+      long_description=read('README.rst'),
       package_dir={'pipe2py': '', 'pipe2py.modules':'modules'},
       packages=['pipe2py', 'pipe2py.modules'],
      )
