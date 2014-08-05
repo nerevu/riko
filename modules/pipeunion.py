82bb28f32c343e419e595aa2ca5ffb6fe6aa30ed
Reuben Cummings <reubano@gmail.com>
2014-08-05 19:18:41 +0300

    Fix whitespace errors and line lengths

--- modules/pipeunion.py
+++ modules/pipeunion.py
@@ -11,22 +11,23 @@ def pipe_union(context, _INPUT, **kwargs):
     _INPUT -- source generator
     kwargs -- _OTHER1 - another source generator
               _OTHER2 etc.
-    
+
     Yields (_OUTPUT):
     union of all source items
     """
-    
+
     #TODO the multiple sources should be pulled in parallel
     # check David Beazely for suggestions (co-routines with queues?)
     # or maybe use multiprocessing and Queues (perhaps over multiple servers too)
     #Single thread and sequential pulling will do for now...
-    
+
     for item in _INPUT:
-        if item == True: #i.e. this is being fed forever, i.e. not a real source so just use _OTHERs
+        #this is being fed forever, i.e. not a real source so just use _OTHERs
+        if item == True:
             break
 
         yield item
-    
+
     for other in kwargs:
         if other.startswith('_OTHER'):
             for item in kwargs[other]:
