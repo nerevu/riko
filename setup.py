c3abb2a9d584a648b0cb040eb7f72c38b1cdbf6a
Reuben Cummings <reubano@gmail.com>
2014-08-05 19:17:10 +0300

    Fix indenting

--- setup.py
+++ setup.py
@@ -9,18 +9,19 @@ def read(fname):
 
 
 setup(
-  name='pipe2py',
-  version='0.10.0',
-  description=(
-    'A project to compile Yahoo! Pipes into Python. '
-    'The pipe2py package can compile a Yahoo! Pipe into pure Python source code,'
-    ' or it can interpret the pipe on-the-fly. It supports embedded pipes too.'
-  ),
-  author='Greg Gaughan',
-  author_email='gjgaughan@gmail.com',
-  url='http://ggaughan.github.com/pipe2py/',
-  license = 'GPL2',
-  long_description=read('README.rst'),
-  package_dir={'pipe2py': '', 'pipe2py.modules':'modules'},
-  packages=['pipe2py', 'pipe2py.modules'],
+    name='pipe2py',
+    version='0.10.0',
+    description=(
+        'A project to compile Yahoo! Pipes into Python. '
+        'The pipe2py package can compile a Yahoo! Pipe into pure Python source '
+        'code, or it can interpret the pipe on-the-fly. It supports embedded '
+        'pipes too.'
+    ),
+    author='Greg Gaughan',
+    author_email='gjgaughan@gmail.com',
+    url='http://ggaughan.github.com/pipe2py/',
+    license = 'GPL2',
+    long_description=read('README.rst'),
+    package_dir={'pipe2py': '', 'pipe2py.modules':'modules'},
+    packages=['pipe2py', 'pipe2py.modules'],
 )
