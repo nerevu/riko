750ff74e17a51572f0cd9c19eeb811b34b6e19bb
Reuben Cummings <reubano@gmail.com>
2015-01-20 11:45:18 +0300

    Bump to version 0.22.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ sys.dont_write_bytecode = True
 
 setup(
     name='pipe2py',
-    version='0.21.0',
+    version='0.22.0',
     description=(
         'A project to compile Yahoo! Pipes into Python. '
         'The pipe2py package can compile a Yahoo! Pipe into pure Python source'
