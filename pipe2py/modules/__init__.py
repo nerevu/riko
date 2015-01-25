8be3386c47a0219fe3e528893978585c10aec2b2
Reuben Cummings <reubano@gmail.com>
2015-01-25 16:15:27 +0300

    Add custom function support

--- pipe2py/modules/__init__.py
+++ pipe2py/modules/__init__.py
@@ -93,18 +93,24 @@ def _get_broadcast_funcs(pieces, ftype='with', **kwargs):
     test = kwargs.pop('pass_if', None)
     listize = kwargs.pop('listize', True)
     parse = kwargs.pop('parse', True)
-    get_value = partial(utils.get_value, **kwargs)
+    cust_func = kwargs.pop('cust_func', False)
+    get_value = None if cust_func else partial(utils.get_value, **kwargs)
     get_pass = partial(utils.get_pass, test=test)
     get_with = partial(utils.get_with, **kwargs)
-    parse_conf = partial(utils.parse_conf, parse_func=get_value, **kwargs)
-    func = parse_conf if parse else get_value
+
+    if cust_func:
+        get_func = cust_func
+    elif parse:
+        get_func = partial(utils.parse_conf, parse_func=get_value, **kwargs)
+    else:
+        get_func = get_value
 
     if listize:
         piece_defs = map(DotDict, utils.listize(pieces))
-        get_pieces = lambda item: imap(func, piece_defs, repeat(item))
+        get_pieces = lambda item: imap(get_func, piece_defs, repeat(item))
     else:
         piece_defs = DotDict(pieces)
-        get_pieces = partial(func, piece_defs)
+        get_pieces = partial(get_func, piece_defs)
 
     return (get_pieces, get_with, get_pass)
 
