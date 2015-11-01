9daec254f3a14c1567ff2b4c3b39e5b3780163dd
Reuben Cummings <reubano@gmail.com>
2015-11-01 13:34:53 +0300

    Fix unicode errors

--- pipe2py/modules/pipestrtransform.py
+++ pipe2py/modules/pipestrtransform.py
@@ -21,7 +21,8 @@ opts = {'listize': False}
 def parse_result(conf, word, _pass):
     allowed = {'capitalize', 'lower', 'upper', 'swapcase', 'title'}
     _pass = _pass if conf.transformation in allowed else True
-    return word if _pass else getattr(str, conf.transformation)(word)
+    encoded = str(word.encode('utf-8')) if isinstance(word, unicode) else word
+    return word if _pass else getattr(str, conf.transformation)(encoded)
 
 
 # Async functions
