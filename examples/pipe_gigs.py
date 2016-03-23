07312050b80627eb67c745a4f7c4123e6ac88cc5
Reuben Cummings <reubano@gmail.com>
2016-03-24 02:14:24 +0300

    Update examples

--- examples/pipe_gigs.py
+++ examples/pipe_gigs.py
@@ -1,29 +1,37 @@
 from __future__ import (
     absolute_import, division, print_function, unicode_literals)
 
-from riko import modules as mod
-from riko.lib.collections import SyncPipe
+from pprint import pprint
+from twisted.internet.defer import inlineCallbacks
+from riko.lib.collections import SyncPipe, SyncCollection
+from riko.twisted.collections import AsyncPipe, AsyncCollection
+
+p68_conf = {'url': 'file://data/gigs.json', 'path': 'value.items'}
+p90_conf = {'uniq_key': 'link'}
+p87_conf = {'combine': 'or', 'mode': 'block', 'rule': [{'field': 'title', 'value': 'php', 'op': 'contains'}]}
+p101_conf = {'rule': [{'sort_key': 'pubDate', 'sort_dir': 'desc'}]}
 
 
-def pipe_gigs(*args, **kwargs):
-    p68_conf={
-        'URL': [
-            {'value': 'http://www.guru.com/rss/jobs/c/web-software-it/'},
-            {'value': 'https://www.elance.com/r/rss/jobs/cat-it-programming/fxd-true/o-1/bgt-gt500-ns1/sct-database-development-10217-data-analysis-14174-database-administration-14177-business-intelligence-14173-data-engineering-14175-system-administration-10219-other-data-science-14178-technical-support-10218-other-it-programming-12350-software-application-10216-website-design-10225-web-programming-10224/tls-1/s-timelistedSort'}]}
+def pipe_gigs(test=False):
+    output = (SyncPipe('fetchdata', conf=p68_conf, test=test)
+        .uniq(conf=p90_conf)
+        .filter(conf=p87_conf)
+        .sort(conf=p101_conf)
+        .list)
 
-    p90_conf = {'field': 'link'}
-    p87_conf = {'COMBINE': 'or', 'MODE': 'block', 'rule': [{'field': 'title', 'value': 'php', 'op': 'contains'}]}
-    p101_conf = {'KEY': [{'field': 'pubDate', 'dir': 'desc'}]}
+    for i in output:
+        pprint(i)
 
-    p101 = (
-        SyncPipe('fetch', conf=p68_conf)
-            .uniq(conf=p90_conf)
-            .filter(conf=p87_conf)
-            .sort(conf=p101_conf)
-            .output)
+    return output
 
-    return p101
 
-if __name__ == "__main__":
-    for i in pipe_gigs():
-        print(i)
+@inlineCallbacks
+def asyncPipeGigs(reactor, test=False):
+    output = yield (AsyncPipe('fetchdata', conf=p68_conf, test=test)
+        .uniq(conf=p90_conf)
+        .filter(conf=p87_conf)
+        .sort(conf=p101_conf)
+        .output)
+
+    for i in output:
+        pprint(i)
---
+++ examples/pipe_simple1.py
@@ -0,0 +1,31 @@
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+
+from pprint import pprint
+from twisted.internet.defer import inlineCallbacks
+from riko.lib.collections import SyncPipe, SyncCollection
+from riko.twisted.collections import AsyncPipe, AsyncCollection
+
+p163_conf = {'attrs':[{'value': 'http://www.caltrain.com/Fares/farechart.html', 'key': 'url'}]}
+p134_conf = {'rule': {'field': 'url', 'match': {'subkey': 'url'}, 'replace': 'farechart'}}
+
+
+def pipe_simple1(test=False):
+    output = (SyncPipe('itembuilder', conf=p163_conf, test=test)
+        .regex(conf=p134_conf)
+        .list)
+
+    for i in output:
+        pprint(i)
+
+    return output
+
+
+@inlineCallbacks
+def asyncPipeSimple1(reactor, test=False):
+    output = yield (AsyncPipe('itembuilder', conf=p163_conf, test=test)
+        .regex(conf=p134_conf)
+        .list)
+
+    for i in output:
+        pprint(i)
