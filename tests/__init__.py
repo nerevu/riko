c6a7f97cc35902b4524f0d4e373d92e92523a125
Reuben Cummings <reubano@gmail.com>
2016-03-25 22:34:52 +0300

    Add unit tests

---
+++ tests/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4:ts=4:expandtab
+"""
+tests
+~~~~~
+
+Provides application unit tests
+"""
+from __future__ import (
+    absolute_import, division, print_function, with_statement,
+    unicode_literals)
+
+from builtins import *
+
+
+def setup_package():
+    """database context creation"""
+    global initialized
+    initialized = True
+    print('Test Package Setup\n')
+
+
+def teardown_package():
+    """database context removal"""
+    global initialized
+    initialized = False
+    print('Test Package Teardown\n')
